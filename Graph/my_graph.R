b8c3553798232e224b08e07f9ee11c0ad57c18ac
SaraDMJN328 <59487543+SaraDMJN328@users.noreply.github.com>
2020-03-23 14:02:12 -0400

    Trying geom_col instead
    .

--- Graph/my_graph.R
+++ Graph/my_graph.R
@@ -32,13 +32,26 @@ labs(title= "Female and Male CEO's in Canada by Indsutry")
 
 #fixing graph
   tab %>% 
-    select(Gender, Industry, VALUE, Unit.of.measure) %>% 
+    select(Gender, VALUE, Industry, Unit.of.measure, REF_DATE) %>% 
     filter(Unit.of.measure == "Number") %>%
+<<<<<<< Updated upstream
     filter(tab, Industry=="Total, all industries")
   tab %>% 
     filter(Industry!="Total, all industries") %>% 
     ("REF_DATE"=="2016") %>%
     ggplot(.,aes(x=Industry,y=VALUE,fill=Gender)) +
     geom_bar() 
+=======
+    filter(Industry!="Total, all industries") %>%
+    filter(REF_DATE=="2016") %>%
+    ggplot(.,aes(x=Industry,y=VALUE,fill=Gender)) +
+    geom_col() +
+      coord_flip()+
+      scale_fill_manual(values=c("hotpink", "deepskyblue2"))+
+      labs(title= "Female and Male CEO's in Canada by Indsutry")
+
+
+## So clean those lines up by deleting stuff that is commented out, add in your lines setting colors and titles doing the coord_flip()
+>>>>>>> Stashed changes
    
   
